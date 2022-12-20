164b47d8957780fd6e4effd6114663b2b93dcce2
Sarah Raguin <sarahraguin@gmail.com>
2022-12-20 11:01:59 -0500

    Working on form input and regex

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -18,6 +18,7 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
+  getFromLocalStorage();
 }
 
 // close modal element
@@ -29,20 +30,25 @@ function closeModal() {
 }
 
 // Regex
-const nameRegex = /^.{2,}$/; // Au moins deux lettres
+const nameRegex = /^[a-zA-Z]{2,}([^0-9]*)$/; // Au moins deux lettres, pas de chiffre
 const emailRegex = /^((?!\.)[\w_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/; // Pas de . en debut ou fin mais ok au milieu, ok pour double nom de domaine, pas d'espace, pas de cq speciaux
 const quantityRegex = /^[0-9]*$/; // Que des chiffres
 
 function regexValidation() {
   if (!nameRegex.test(document.getElementById("first").value)) {
     window.alert("Une erreur s'est glissée dans votre formulaire au niveau du prénom");
-  } else if(!nameRegex.test(document.getElementById("last").value)) {
+  }
+  if(!nameRegex.test(document.getElementById("last").value)) {
     window.alert("Une erreur s'est glissée dans votre formulaire au niveau du nom");
-  } else if(!emailRegex.test(document.getElementById("email").value)) {
+  }
+  if(!emailRegex.test(document.getElementById("email").value)) {
     window.alert("Une erreur s'est glissée dans votre formulaire au niveau de l'email");
-  } else if(!quantityRegex.test(document.getElementById("quantity").value)) {
+  }
+  if(!quantityRegex.test(document.getElementById("quantity").value)) {
     window.alert("Une erreur s'est glissée dans votre formulaire au niveau de la quantité");
-  } else {
+  }
+  if(nameRegex.test(document.getElementById("first").value) && nameRegex.test(document.getElementById("last").value)
+  && emailRegex.test(document.getElementById("email").value) && quantityRegex.test(document.getElementById("quantity").value)) {
     return true;
   }
 };
@@ -58,17 +64,42 @@ function radioSelected() {
 
 // Checkbox1 is selected
 function checkboxSelected() {
-  if(document.getElementById('input[id="checkbox1"]:checked') == null) {
+  if(!document.getElementById("checkbox1").checked) {
   window.alert("Vous devez accepter nos conditions d'utilisation pour pouvoir valider le formulaire !");
   } else {
     return true;
   }
 };
 
+// Retrieve elements from localStorage
+function getFromLocalStorage() {
+  modalbg.style.display = "block";
+  const first = localStorage.getItem("firstName");
+  const last = localStorage.getItem("lastName");
+  const email = localStorage.getItem("email");
+  const birthdate = localStorage.getItem("birthDate");
+  const quantity = localStorage.getItem("quantity");
+
+  document.getElementById("first").value = first;
+  document.getElementById("last").value = last;
+  document.getElementById("email").value = email;
+  document.getElementById("birthdate").value = birthdate;
+  document.getElementById("quantity").value = quantity;
+}
+
 // Validation on submit
 function validate() {
+
+  // Keep elements of form in localStorage
+  localStorage.setItem("firstName", document.getElementById("first").value);
+  localStorage.setItem("lastName", document.getElementById("last").value);
+  localStorage.setItem("email", document.getElementById("email").value);
+  localStorage.setItem("birthDate", document.getElementById("birthdate").value);
+  localStorage.setItem("quantity", document.getElementById("quantity").value);
+
   if (regexValidation() && radioSelected() && checkboxSelected()) {
     window.alert("Votre formulaire a bien été envoyé à nos services");
+    localStorage.clear();
   } else {
     window.alert("Une erreur s'est produite lors de l'envoi du formulaire");
   }
