02d829fd4dfa7c628f0c9ed5c1b24830d143d032
Sarah Raguin <sarahraguin@gmail.com>
2022-12-20 15:32:22 -0500

    Testing and changing errors

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -32,27 +32,27 @@ function closeModal() {
 // Regex
 const nameRegex = /^[a-zA-Z]{2,}([^0-9]*)$/; // Au moins deux lettres, pas de chiffre
 const emailRegex = /^((?!\.)[\w_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/; // Pas de . en debut ou fin mais ok au milieu, ok pour double nom de domaine, pas d'espace, pas de cq speciaux
-const birthDateRegex = /\s+(?:0[1-9]|1[012])[-/.](?:0[1-9]|[12][0-9]|3[01])[-/.](?:19\d{2}|20[01][0-9]|2020)\b/;
+const birthdateRegex = /^\d{4}\-(0?[1-9]|1[012])\-(0?[1-9]|[12][0-9]|3[01])$/; // YYYY/MM/DD
 const quantityRegex = /^[0-9]*$/; // Que des chiffres
 
 function regexValidation() {
-  if (!nameRegex.test(document.getElementById("first").value)) {
-    window.alert("Veuillez entrer 2 caractères ou plus pour le champ du prénom.");
+  if (!nameRegex.test(document.getElementById("first").value) || document.getElementById("first").value == "") {
+    document.getElementById("errorFirst").textContent="Veuillez entrer 2 caractères ou plus pour le champ du prénom.";
   }
-  if(!nameRegex.test(document.getElementById("last").value)) {
-    window.alert("Veuillez entrer 2 caractères ou plus pour le champ du nom.");
+  if(!nameRegex.test(document.getElementById("last").value) || document.getElementById("last").value == "") {
+    document.getElementById("errorLast").textContent="Veuillez entrer 2 caractères ou plus pour le champ du nom.";
   }
-  if(!emailRegex.test(document.getElementById("email").value)) {
-    window.alert("Une erreur s'est glissée dans votre formulaire au niveau de l'email");
+  if(!emailRegex.test(document.getElementById("email").value) || document.getElementById("email").value == "") {
+    document.getElementById("errorEmail").textContent="Une erreur s'est glissée dans votre formulaire au niveau de l'email.";
   }
-  if(!birthDateRegex.test(document.getElementById("birthdate").value)) {
-    window.alert("Vous devez entrer votre date de naissance.");
+  if(!birthdateRegex.test(document.getElementById("birthdate").value) || document.getElementById("birthdate").value == "") {
+    document.getElementById("errorBirthdate").textContent="Vous devez entrer votre date de naissance";
   }
-  if(!quantityRegex.test(document.getElementById("quantity").value)) {
-    window.alert("Une erreur s'est glissée dans votre formulaire au niveau de la quantité");
+  if(!quantityRegex.test(document.getElementById("quantity").value) || document.getElementById("quantity").value == "") {
+    document.getElementById("errorQuantity").textContent="Une erreur s'est glissée dans votre formulaire au niveau de la quantité";
   }
-  if(nameRegex.test(document.getElementById("first").value) && nameRegex.test(document.getElementById("last").value)
-  && emailRegex.test(document.getElementById("email").value) && quantityRegex.test(document.getElementById("bithdate").value) 
+  if(nameRegex.test(document.getElementById("first").value) && nameRegex.test(document.getElementById("last").value) 
+  && emailRegex.test(document.getElementById("email").value) && birthdateRegex.test(document.getElementById("birthdate").value) 
   && quantityRegex.test(document.getElementById("quantity").value)) {
     return true;
   }
@@ -82,7 +82,7 @@ function getFromLocalStorage() {
   const first = localStorage.getItem("firstName");
   const last = localStorage.getItem("lastName");
   const email = localStorage.getItem("email");
-  const birthdate = localStorage.getItem("birthDate");
+  const birthdate = localStorage.getItem("birthdate");
   const quantity = localStorage.getItem("quantity");
 
   document.getElementById("first").value = first;
@@ -99,13 +99,15 @@ function validate() {
   localStorage.setItem("firstName", document.getElementById("first").value);
   localStorage.setItem("lastName", document.getElementById("last").value);
   localStorage.setItem("email", document.getElementById("email").value);
-  localStorage.setItem("birthDate", document.getElementById("birthdate").value);
+  localStorage.setItem("birthdate", document.getElementById("birthdate").value);
   localStorage.setItem("quantity", document.getElementById("quantity").value);
 
   if (regexValidation() && radioSelected() && checkboxSelected()) {
     window.alert("Votre formulaire a bien été envoyé à nos services");
     localStorage.clear();
+    return true;
   } else {
     window.alert("Une erreur s'est produite lors de l'envoi du formulaire");
+    return false;
   }
 };
